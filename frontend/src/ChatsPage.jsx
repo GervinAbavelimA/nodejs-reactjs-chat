8615136657fe156f259c4d1b0824dfa6d8a5898d
GervinAbavelimA <abavelimgervin@gmail.com>
2023-12-20 12:25:58 -0400

    add

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,16 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+    return ( 
+    <div style={{ height: '100vh'}}>
+        <PrettyChatWindow
+        projectId='d91cf5dc-0fbb-4da7-b97e-cd3ea15eab7c'
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: '100%'}}
+        />
+    </div>
+    )
 }
 
 export default ChatsPage;
